4dad2637c5556c2966c02e28ca30036249d7e7ed
sammiroy <90281906+sammiroy@users.noreply.github.com>
2021-12-14 12:47:47 -0700

    Add files via upload

---
+++ ClassClown/Backpack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassClown
+{
+	public class Backpack
+	{
+		// Fields
+		private string color;
+		private string size;
+		private List<string> items = new List<string>();
+		private bool open;
+
+		// Constructor
+		public Backpack(string Color, string Size)
+		{
+			color = Color;
+			size = Size;
+			items = new List<string>();
+			open = false;
+		}
+
+		// Methods
+		public void OpenBag()
+        {
+			this.open = true;
+        }
+
+		public void CloseBag()
+        {
+			this.open = false;
+        }
+
+		public void PutIn(string item)
+        {
+			item = item.ToLower();
+			if (!open)
+			{
+				Console.WriteLine($"Cannot add {item} to the {this.size}, {this.color} backpack, it is closed");
+				return;
+			}
+			this.items.Add(item);
+			Console.WriteLine($"Added {item} to the {this.size}, {this.color} backpack");
+        }
+
+		public void TakeOut(string item)
+        {
+			item = item.ToLower();
+			if (!open)
+            {
+				Console.WriteLine($"Cannot take {item} from the {this.size}, {this.color} backpack, it is closed");
+				return;
+			} else if (!this.items.Contains(item))
+            {
+				Console.WriteLine($"Cannot take {item} from the {this.size}, {this.color} backpack, it is not in this backpack!");
+				return;
+			}
+			this.items.Remove(item);
+			Console.WriteLine($"Removed {item} from the {this.size}, {this.color} backpack");
+
+		}
+	}
+}
---
+++ ClassClown/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassClown // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Character Class stuff
+            // Init Character Objects
+            Character char1 = new Character("Patrick Bateman", "Look at paul allen's card", "why not you stupid [censored]");
+            Character char2 = new Character("Mario", "Wahoo", "I'm christ pratt");
+
+            // Test Character functionality
+            // Character 1
+            char1.SayPrimaryPhrase();
+            char1.SaySecondaryPhrase();
+            char1.LevelUp();
+            char1.SayPrimaryPhrase();
+            char1.SaySecondaryPhrase();
+            // Character 2 
+            char2.SayPrimaryPhrase();
+            char2.SaySecondaryPhrase();
+            char2.LevelUp();
+            char2.SayPrimaryPhrase();
+            char2.SaySecondaryPhrase();
+
+            // Backpack Class Stuff
+            // Init Backpack Objects
+            Backpack bkpk1 = new Backpack("blue", "small");
+            Backpack bkpk2 = new Backpack("red", "medium");
+            Backpack bkpk3 = new Backpack("green", "large");
+
+            // Test Backpack functionality
+            bkpk1.OpenBag();
+            bkpk1.PutIn("lunch");
+            bkpk1.PutIn("jacket");
+            bkpk1.CloseBag();
+            bkpk1.OpenBag();
+            bkpk1.TakeOut("jacket");
+            bkpk1.CloseBag();
+
+        }
+    }
+}
